d4cd8c8710ae6d010de723a27da9a930cc5e39b8
Solar Designer <solar@openwall.com>
2019-03-11 19:19:41 +0100

    Drop last 2 elements of keccakf_rndc[] since we use a 22-round Keccak variant

--- progpow.c
+++ progpow.c
@@ -76,11 +76,11 @@ static uint32_t bswap(uint32_t a)
 // Implementation based on:
 // https://github.com/mjosaarinen/tiny_sha3/blob/master/sha3.c
 
-static const uint32_t keccakf_rndc[24] = {
+static const uint32_t keccakf_rndc[22] = {
     0x00000001, 0x00008082, 0x0000808a, 0x80008000, 0x0000808b, 0x80000001,
     0x80008081, 0x00008009, 0x0000008a, 0x00000088, 0x80008009, 0x8000000a,
     0x8000808b, 0x0000008b, 0x00008089, 0x00008003, 0x00008002, 0x00000080,
-    0x0000800a, 0x8000000a, 0x80008081, 0x00008080, 0x80000001, 0x80008008
+    0x0000800a, 0x8000000a, 0x80008081, 0x00008080
 };
 
 // Implementation of the Keccakf transformation with a width of 800
